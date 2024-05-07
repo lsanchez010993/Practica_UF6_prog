8429ccc0009f478d4b4d49743a4b579e70cda3cc
Alberto <a.morcillo@sapalomera.cat>
2024-05-07 12:45:40 +0200

    Estructura del menú principal hecho

--- main.java
+++ main.java
@@ -1,5 +1,75 @@
+import java.util.InputMismatchException;
+import java.util.Scanner;
+import java.util.Scanner;
+
 public class main {
+    static Scanner scan = new Scanner(System.in);
+
     public static void main(String[] args) {
-        System.out.println("Hola, que tal?");
+
+        mostrarMenuInicial();
+    }
+
+    public static void mostrarMenuInicial() {
+        int opcion = -1;
+        do {
+            try {
+                System.out.println("\n--------MENU INICIAL--------");
+                System.out.println("1. Llistar tots els jugadors d'un equip.");
+                System.out.println("2. Calcular la mitjana de punts, rebots, asistències,... d'un jugador.");
+                System.out.println("3. Llistar tots els partirs jugats per un equip amb el seu resultat.");
+                System.out.println("4. Inserir un nou jugador a un equip.");
+                System.out.println("5. Traspassar un jugador a un altra equip.");
+                System.out.println("6. Actualitzar les dades de jugadors o equips després d'un partit.");
+                System.out.println("7. Modificar les estadístiques d'un jugador.");
+                System.out.println("8. Retirar (Eliminar) un jugador.");
+                System.out.println("9. Canviar nom franquícia d'un equip.");
+                System.out.println("0. Sortir de l'aplicació.");
+                System.out.print("\nElige qué quieres hacer: ");
+
+                opcion = scan.nextInt();
+                scan.nextLine(); // Limpiar el búfer
+
+                switch (opcion) {
+                    case 1:
+
+                        break;
+                    case 2:
+
+                        break;
+                    case 3:
+
+                        break;
+                    case 4:
+
+                        break;
+                    case 5:
+
+                        break;
+                    case 6:
+
+                        break;
+                    case 7:
+
+                        break;
+                    case 8:
+
+                        break;
+                    case 9:
+                        break;
+
+                    case 0:
+                        System.out.println("Saliendo de la aplicación...");
+                        break;
+                    default:
+                        System.out.println("¡ATENCIÓN! Debe ser un valor entre 0 y 5.");
+                }
+            } catch (InputMismatchException e) {
+                System.out.println("Error: Entrada inválida. Por favor, introduce un número entero.");
+                scan.nextLine(); // Limpiar el búfer
+            } catch (Exception e) {
+                System.out.println("Ocurrió un error genérico.");
+            }
+        } while (opcion != 0);
     }
 }
