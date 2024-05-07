7bfd60df7f9d9df35871ea0cef43da059213cb92
luis0 <luis010993@gmail.com>
2024-05-07 13:43:27 +0200

    DatabaseConnection

--- modelo/DatabaseConnection.java
+++ modelo/DatabaseConnection.java
@@ -1,12 +1,13 @@
 package modelo;
+
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class DatabaseConnection {
-    private static final String URL = "jdbc:mysql://localhost:3306/nombreDeTuBaseDeDatos"; // Ajusta el puerto si es necesario y el nombre de la base de datos
-    private static final String USER = "tuUsuario";  // Reemplaza con tu usuario
-    private static final String PASSWORD = "tuContraseña";  // Reemplaza con tu contraseña
+    private static final String URL = "jdbc:mysql://localhost:3306/nba"; // Ajusta el puerto si es necesario y el nombre de la base de datos
+    private static final String USER = "perepi";  // Reemplaza con tu usuario
+    private static final String PASSWORD = "pastanaga";  // Reemplaza con tu contraseña
     private static final String DRIVER = "com.mysql.cj.jdbc.Driver"; // Esto es el driver de JDBC para MySQL
 
     public static Connection getConnection() {
