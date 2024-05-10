2c931fffc0f7603e3c32f60e8c68542d117d7dd2
Alberto <a.morcillo@sapalomera.cat>
2024-05-10 11:27:42 +0200

    Añadido las clases de servicio que se ocupara de recibir etc datos json

---
+++ servicio/PlayerDataService.java
@@ -0,0 +1,17 @@
+package servicio;
+
+import dao.PlayerDAO;
+import modelo.Player;
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
+import java.io.FileReader;
+import java.io.IOException;
+
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.Scanner;
+
+public class PlayerDataService {
+}
